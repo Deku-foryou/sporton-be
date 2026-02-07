c43f39b55513d2fc9244e8e400ef7918a6617eaf
Deku-foryou <reyhanmagribi57@gmail.com>
2026-02-07 17:57:07 +0700

    integrasi be api product categorty live sesssion 7

---
+++ src/middlewares/upload.middleware.ts
@@ -0,0 +1,36 @@
+import Multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const uploadDir = "uploads";
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+const storage = Multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const fileFilter = (
+  req: any,
+  file: Express.Multer.File,
+  cb: Multer.FileFilterCallback,
+) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed!"));
+  }
+};
+
+export const upload = Multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB limit
+});
